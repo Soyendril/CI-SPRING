ecce6a9f90c7ece1022d5464661165c452657eb4
Soyendril <souleymane.rakkassy@gmail.com>
2023-05-03 12:10:51 +0200

    mise en comment du failure test

--- src/test/java/com/example/springunitaire/TodoService/TodoServiceTest.java
+++ src/test/java/com/example/springunitaire/TodoService/TodoServiceTest.java
@@ -114,7 +114,8 @@ public class TodoServiceTest {
         assertThat(existingTodo.isDone(), is(true));
     }
 
-/*    @Test
+  @Test
+  /*
     public void testSomething() {
         // Échoue le test délibérément
         fail("Ce test a échoué intentionnellement");
